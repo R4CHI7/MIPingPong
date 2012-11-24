faa9560eb5e90193c50e6a3f47aad205f0522bd7
R4CHI7 <rachitgupta1792@gmail.com>
2012-11-24 16:29:22 +0530

    Updated The Graphics Functioning

--- src/com/multi/pingpong/MainActivity.java
+++ src/com/multi/pingpong/MainActivity.java
@@ -3,16 +3,126 @@ package com.multi.pingpong;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 
 public class MainActivity extends Activity {
     /** Called when the activity is first created. */
-	Main_Canvas obj ;
+	Main_Canvas gameCanvas ;
+	int power,power1,power2;
+	float downx,downy,movex,movey;
+	float downx1,downy1,movex1,movey1;
+	float downx2,downy2,movex2,movey2;
+	int temp,temp1,temp2;
+	
+	
+	Handler updateHandler = new Handler() {
+		  @Override
+		  public void handleMessage(Message msg) {
+		//display each item in a single line
+			  gameCanvas.update();
+			  //gameCanvas.invalidate();
+	            super.handleMessage(msg);
+		     }
+		 };
+	
+	
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    	obj = new Main_Canvas(this) ;
+    	gameCanvas = new Main_Canvas(this) ;
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        setContentView(obj);
+        setContentView(gameCanvas);
+        temp = 0;
+        gameCanvas.setOnTouchListener(new OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				// TODO Auto-generated method stub
+				int action = event.getAction();
+				if(event.getY() < gameCanvas.height/2){
+				switch(action){
+				case MotionEvent.ACTION_DOWN:
+					power1 = 0;
+					temp1 = gameCanvas.pad1x;
+					downx1 = event.getX();
+					downy1 = event.getY();
+					break;
+				case MotionEvent.ACTION_MOVE:
+					movex1 = event.getX();
+					movey1 = event.getY();
+					power1 = (int) (movex1-downx1);
+					gameCanvas.pad1x = power1 + temp1;
+					break;
+				case MotionEvent.ACTION_UP:
+					power1 = 0;
+					break; 
+				}
+				}
+				else if(event.getY() > gameCanvas.height/2){
+					switch(action){
+					case MotionEvent.ACTION_DOWN:
+						power2 = 0;
+						temp2 = gameCanvas.pad2x;
+						downx2 = event.getX();
+						downy2 = event.getY();
+						break;
+					case MotionEvent.ACTION_MOVE:
+						movex2 = event.getX();
+						movey2 = event.getY();
+						power2 = (int) (movex2-downx2);
+						gameCanvas.pad2x = power2 + temp2;
+						break;
+					case MotionEvent.ACTION_UP:
+						power2 = 0;
+						break; 
+					}
+					}
+				/*
+				 //For multiplayer
+				   switch(action){
+					case MotionEvent.ACTION_DOWN:
+						power = 0;
+						temp = gameCanvas.pad1x;
+						downx = event.getX();
+						downy = event.getY();
+						break;
+					case MotionEvent.ACTION_MOVE:
+						movex = event.getX();
+						movey = event.getY();
+						power = (int) (movex-downx);
+						gameCanvas.pad1x = power + temp;
+						break;
+					case MotionEvent.ACTION_UP:
+						power = 0;
+						break;
+				}
+				*/
+				return true;
+			}
+		});
+        
+        
+        Thread ball_movement_thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				 while(true){
+	                 updateHandler.sendEmptyMessage(0);
+	                 try {
+						Thread.sleep(2);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				 }
+			}
+		});
+        ball_movement_thread.start();
+
     }
 }
--- src/com/multi/pingpong/Main_Canvas.java
+++ src/com/multi/pingpong/Main_Canvas.java
@@ -4,25 +4,34 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.util.AttributeSet;
 import android.view.View;
 
 public class Main_Canvas extends View {
 	
-	private int height, width ;
+	public int height;
+	public int width ;
 	private Paint paint ;
-	private int ballr, rect_width, goal_width_half ; 
+	private int ballr, rect_width, goal_width_half,paddle_width_half ; 
+	private int right_wall, left_wall, top_wall, bottom_wall,speed,sx,sy;
+	public int half_width;
+	private int ballx, bally;
 	private float ratio ;
 	private boolean flag ;
+	public int pad1x,pad2x;
+	int zoom;
 
+	
+	Thread t=null;
 	public Main_Canvas(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
 		paint = new Paint() ;
 		paint.setAntiAlias(true) ;
-		ballr = 5 ;
-		rect_width = 7 ;
-		goal_width_half = 50 ;
+		zoom = 2;
+		ballr = 7*zoom ;
+		rect_width = 10*zoom ;
+		goal_width_half = 70*zoom ;
+		pad1x=pad2x=0;
 		flag = false ;
 	}
 	
@@ -47,24 +56,83 @@ public class Main_Canvas extends View {
 
 	private void render(Canvas canvas) {
 		// TODO Auto-generated method stub
+		
+		//seeting background color
 		paint.setColor(Color.BLACK) ;
 		canvas.drawPaint(paint) ;
+		
+		//setting white walls
 		paint.setColor(Color.WHITE) ;
 		canvas.drawRect(0, 0, width, height, paint) ;
+		//setting middle area and goals
 		paint.setColor(Color.BLACK) ;
 		canvas.drawRect(rect_width, rect_width, (width-rect_width), (height-rect_width), paint) ;
 		canvas.drawRect((width/2)-goal_width_half, 0, (width/2)+goal_width_half, rect_width, paint) ;
 		canvas.drawRect((width/2)-goal_width_half, height-rect_width, (width/2)+goal_width_half, height, paint) ;
+		
+		//setting paddles
+		paint.setColor(Color.WHITE);
+		if(pad1x+width/2 - paddle_width_half < rect_width+ballr/2)
+			pad1x = paddle_width_half + rect_width - (width -ballr)/2;
+		else if(pad1x+ width/2 + paddle_width_half > width - rect_width - ballr/2)
+			pad1x = -paddle_width_half -rect_width+(width -ballr)/2;
+		canvas.drawRect(pad1x+width/2 - paddle_width_half, rect_width*2,pad1x+ width/2 + paddle_width_half, rect_width*3, paint);
+		if(pad2x+width/2 - paddle_width_half < rect_width+ballr/2)
+			pad2x = paddle_width_half + rect_width - (width -ballr)/2;
+		else if(pad2x+ width/2 + paddle_width_half > width - rect_width - ballr/2)
+			pad2x = -paddle_width_half -rect_width+(width -ballr)/2;
+		canvas.drawRect(pad2x+width/2 - paddle_width_half, height - rect_width*3,pad2x+width/2 + paddle_width_half, height - rect_width*2, paint);
+		
+		
+		//ball
+		paint.setColor(Color.YELLOW);
+		canvas.drawCircle(ballx, bally, ballr, paint);
+		ballx++;
+		bally++;
+		try {
+			Thread.sleep(10,00);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	private void init(Canvas canvas) {
 		// TODO Auto-generated method stub
 		height = canvas.getHeight() ;
 		width = canvas.getWidth() ;
+		half_width = width/2;
 		ratio = height/width ;
 		ballr*=ratio ;
 		rect_width*=ratio ;
 		goal_width_half*=ratio ;
+		paddle_width_half = (goal_width_half - 6*ballr);
+		ballx = width/2;
+		bally = height/2;
+		left_wall = rect_width;
+		right_wall = width - rect_width;
+		top_wall = rect_width;
+		bottom_wall = height - rect_width;
+		speed=1;
+		sx=sy=speed;
 		flag = true ;
 	}
+	
+	// called by thread
+    public void update() {
+    	if(ballx-speed <= left_wall) sx=speed;
+    	else if(ballx+speed >= right_wall) sx=-speed;
+    	if(bally-speed <= top_wall+2*rect_width 
+    			&& (ballx >=pad1x + half_width - paddle_width_half && ballx<=pad1x+half_width+paddle_width_half))sy=speed;
+    	else if(bally-speed <= top_wall  
+    			&& !(ballx >=half_width-paddle_width_half && ballx<=half_width+paddle_width_half))
+    		sy=speed;
+    	else if(bally+speed >= bottom_wall-2*rect_width 
+    			&& (ballx >=pad2x + half_width - paddle_width_half && ballx<=pad2x+half_width+paddle_width_half))sy=-speed;
+    	else if(bally+speed >= bottom_wall 
+    			&& !(ballx >=half_width-paddle_width_half && ballx<=half_width+paddle_width_half))
+    		sy=-speed;	
+        ballx+=sx;
+        bally+=sy;
+    }
 }
